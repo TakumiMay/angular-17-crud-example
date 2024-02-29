2220fc5b116f45d58f708c0695af7e2610875dc4
MAYUMI TAMURA HERNANDEZ <mtamura@quala.com.co>
2024-02-29 11:42:33 -0500

    Add new tutorial cypress test added

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,19 +1,19 @@
 describe('home page', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:8081/api/tutorials');
-  });
-
-  it('has the correct text', () => {
-    cy.title().should('equal', 'Angular17Crud');
-  });
-
   it('add a tutorial', () => {
+    cy.visit('http://localhost:8081/tutorials');
+    cy.title().should('equal', 'Angular17Crud');
+    cy.get('.btn.btn-sm.btn-danger').click();
     cy.get("a.nav-link").eq(1).click();
     cy.url().should('equal', 'http://localhost:8081/add');
     cy.get('#title').clear().type('New tutorial title');
     cy.get('#description').clear().type('New tutorial description');
     cy.get('button.btn.btn-success').click();
-    //cy.url().should('equal', 'http://localhost:8081/tutorials');
+    cy.get("h4").contains("Tutorial was submitted successfully!");
+    cy.get("a.nav-link").eq(0).click();
+    cy.get('input[placeholder="Search by title"]').type('New tutorial title');
+    cy.get('.btn.btn-outline-secondary').click();
+    cy.get("h4").contains("Tutorials List");
+    cy.get('li.list-group-item').contains('New tutorial title').click();
+    cy.get('div').contains('label', 'Description:').parent().contains('New tutorial description');
   });
-
 })
