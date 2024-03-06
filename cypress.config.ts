3b9bf63597b25a4c924d306532f8344ef8f078cc
MAYUMI TAMURA HERNANDEZ <maythermyself@gmail.com>
2024-03-06 12:15:44 -0500

    Mochawesome reports added

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
